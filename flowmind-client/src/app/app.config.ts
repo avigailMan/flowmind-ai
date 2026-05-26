d9e71a8c06428d5d9aed1560e19f63b1e97f10b4
judy759 <yehudit5759@example.com>
2026-05-26 13:49:27 +0300

    fix: remove submodule and add client as regular folder

---
+++ flowmind-client/src/app/app.config.ts
@@ -0,0 +1,21 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { routes } from './app.routes';
+
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+
+    providePrimeNG({
+      theme: {
+         preset: Aura
+      }
+    })
+  ]
+};
