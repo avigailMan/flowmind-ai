dbef1118b296e15f87a136b3548af5524093963b
judy759 <yehudit5759@example.com>
2026-05-27 12:30:56 +0300

    feat(dashboard): create responsive finance dashboard

--- flowmind-client/src/app/features/auth/login/login.ts
+++ flowmind-client/src/app/features/auth/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
@@ -7,6 +7,7 @@ import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
+declare const google: any;
 
 @Component({
   selector: 'app-login',
@@ -24,6 +25,32 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
+export class Login implements AfterViewInit {
   rememberMe = false;
+  showPassword = false;
+  ngAfterViewInit(): void {
+
+    google.accounts.id.initialize({
+      client_id: '943512359144-k2o632t6jnqqv8hhcklaqptlsdkcchea.apps.googleusercontent.com',
+      callback: (response: any) => this.handleGoogleLogin(response)
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById('googleBtn'),
+      {
+        theme: 'outline',
+        size: 'large',
+        width: 420
+      }
+    );
+  }
+
+  handleGoogleLogin(response: any) {
+
+    console.log(response);
+
+    const idToken = response.credential;
+
+    // לשלוח לבקאנד
+  }
 }
